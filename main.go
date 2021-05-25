d07d43def57170bb5e91545cc3c6d6fbc06c569e
clshu <clshu2020@gmail.com>
2021-05-24 22:49:16 -0700

    Adding a 404 page.

--- main.go
+++ main.go
@@ -10,7 +10,10 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Welcome to AA website</h1>")
 	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "<h1>Contact Page</h1><br><h3>Please Contact <a href=\"mailto: support@lenslock.com\">support@lenslock.com</a></h3")
+		fmt.Fprint(w, "<h1>Contact Page</h1><br><h3>Please Contact <a href=\"mailto: support@lenslock.com\">support@lenslock.com</a></h3>")
+	} else {
+		w.WriteHeader(http. StatusNotFound)
+		fmt.Fprint(w, "<h1>Page Not Found</h1><p>Email us if you keep receiving an invalid page.</p><p><a href=\"mailto: support@lenslock.com\">support@lenslock.com</a></p>")
 	}
 }
 
