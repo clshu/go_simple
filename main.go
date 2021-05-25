c70493e397f91d295544321a825d591225a01a69
clshu <clshu2020@gmail.com>
2021-05-25 04:32:02 -0700

    Add FAQ and 404 pages. Using httprouter.

--- main.go
+++ main.go
@@ -4,20 +4,20 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/julienschmidt/httprouter"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to AA website</h1>")
 }
 
-func contact(w http.ResponseWriter, r *http.Request) {
+func contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>Email us at </p><p><a href=\"mailto: support@lenslock.com\">support@lenslock.com</a></p>")
 }
 
-func faq(w http.ResponseWriter, r *http.Request) {
+func faq(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>FAQ</h1><p>What's suuport email? </p><p>Answer: <a href=\"mailto: support@lenslock.com\">support@lenslock.com</a></p>")
 }
@@ -29,10 +29,10 @@ func handle404(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/faq", faq)
-	r.NotFoundHandler = http.HandlerFunc(handle404)
-	http.ListenAndServe(":4000", r)
+	router := httprouter.New()
+	router.GET("/", home)
+	router.GET("/contact", contact)
+	router.GET("/faq", faq)
+	router.NotFound = http.HandlerFunc(handle404)
+	http.ListenAndServe(":4000", router)
 }
