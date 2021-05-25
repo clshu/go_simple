3e16d52446d2b074d36959cb4dc1e13ac1e4c656
clshu <clshu2020@gmail.com>
2021-05-25 03:01:57 -0700

    httpRouter exercise. Add package. Set go formatter as default.

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
@@ -12,14 +14,20 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "<h1>Contact Page</h1><br><h3>Please Contact <a href=\"mailto: support@lenslock.com\">support@lenslock.com</a></h3>")
 	} else {
-		w.WriteHeader(http. StatusNotFound)
+		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1>Page Not Found</h1><p>Email us if you keep receiving an invalid page.</p><p><a href=\"mailto: support@lenslock.com\">support@lenslock.com</a></p>")
 	}
 }
 
+func handleHello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
+
 func main() {
-	mux := &http.ServeMux{}
-	mux.HandleFunc("/", handlerFunc)
+	router := httprouter.New()
+	router.GET("/hello/:name", handleHello)
+	// mux := &http.ServeMux{}
+	// mux.HandleFunc("/", handlerFunc)
 	// http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":4000", mux)
+	http.ListenAndServe(":4000", router)
 }
